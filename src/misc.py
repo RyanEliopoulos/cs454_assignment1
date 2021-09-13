a662cbb8e0496b15c3cc9bb669e1caad5f95cb52
Ryan <ryan.eliopoulos@gmail.com>
2021-09-12 19:32:42 -0700

    +argparse support
    misc now contains the function for building the argparser. Called from
    the main function.

--- src/misc.py
+++ src/misc.py
@@ -1,4 +1,5 @@
 import datetime
+import argparse
 
 
 def historical_date_fix(date_string: str) -> float:
@@ -30,6 +31,15 @@ def api_days() -> tuple[str, str, str, str]:
     return day_of_month, yesterday_day, month, yester_month
 
 
+def argparser() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', action='store_true', help='Initialize/reset the database')
+    parser.add_argument('--symbols', action='store_true', help='Populate stock symbols from nasdaq.com')
+    parser.add_argument('--historical', action='store_true',
+                        help='Load historical data for the symbols currently in the database')
+    parser.add_argument('--prune', action='store_true', help='Removes')
+    parser.add_argument('--wsb', action='store_true', help='Crawl the last 7 days of WSB posts')
+    return parser.parse_args()
 
 
 
